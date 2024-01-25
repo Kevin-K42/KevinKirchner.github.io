4b1601c61cbb8859f0c466936f1ecda445624de8
Kevin Kirchner <84873726+Kevin-K42@users.noreply.github.com>
2024-01-25 20:43:31 +0100

    Add files via upload

--- email.js
+++ email.js
@@ -2,4 +2,5 @@ window.onload = function () {
     const encEmail = "a2V2aW4ua2lyY2huZXI5OEBnbWFpbC5jb20=";
     const form = document.getElementById("contact");
     form.setAttribute("href", "mailto:".concat(atob(encEmail)));
+    form.innerHTML=atob(encEmail);
 };
